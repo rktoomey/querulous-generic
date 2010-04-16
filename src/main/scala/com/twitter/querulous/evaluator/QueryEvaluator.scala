e8e121cc846c4723dc7cb0f4c2137f1757ebfd79
Rhys Keepence <rhyskeepence@gmail.com>
2010-04-16 17:03:31 +0100

    Connection validation will only be enabled if a specific validation query is given.

--- src/main/scala/com/twitter/querulous/evaluator/QueryEvaluator.scala
+++ src/main/scala/com/twitter/querulous/evaluator/QueryEvaluator.scala
@@ -8,7 +8,7 @@ import com.twitter.xrayspecs.TimeConversions._
 object QueryEvaluator extends QueryEvaluatorFactory {
   private def createEvaluatorFactory() = {
     val queryFactory = new SqlQueryFactory
-    val databaseFactory = new ApachePoolingDatabaseFactory("select 1 from INFORMATION_SCHEMA.SYSTEM_USERS", 10, 10, 1.second, 10.millis, false, 0.seconds)
+    val databaseFactory = new ApachePoolingDatabaseFactory(null, 10, 10, 1.second, 10.millis, false, 0.seconds)
     new StandardQueryEvaluatorFactory(databaseFactory, queryFactory)
   }
 
